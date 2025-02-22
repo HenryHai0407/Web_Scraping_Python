12b4914d27b3676c57e67502bb3c52b910c8e7f3
HenryHai0407 <hoanghai15100@gmail.com>
2025-02-22 03:12:34 +0200

    to scrape data from website using Selenium

---
+++ web_scraping_selenium.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import csv
+
+# Initialize WebDriver (Make sure to specify the correct path to your ChromeDriver)
+driver = webdriver.Chrome() 
+
+#Open a website
+driver.get("https://books.toscrape.com/catalogue/page-2.html")
+
+# Wait for JavaScript to load (optional)
+time.sleep(3)
+
+products = driver.find_elements(By.CSS_SELECTOR, "h3 a")
+
+# Prepare data to save
+product_titles = [product.get_attribute('title') for product in products]
+
+csv_file = 'product_titles.csv'
+
+# Write data to CSV file
+with open(csv_file, mode='w', newline= '', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Product Titles'])
+    for title in product_titles:
+        writer.writerow([title])
+
+# Close the browser
+driver.quit()
+
+print(f'Successfully saved {len(product_titles)} titles of product to {csv_file}.')
