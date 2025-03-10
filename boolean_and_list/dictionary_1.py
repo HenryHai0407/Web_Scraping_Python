6747c4cada8a3ad4a048bc2b0c21a21324f418f4
HenryHai0407 <hoanghai15100@gmail.com>
2025-03-11 01:45:02 +0200

    update dic_1.py

--- boolean_and_list/dictionary_1.py
+++ boolean_and_list/dictionary_1.py
@@ -1,15 +1,47 @@
-car = {
-"brand": "BMW",
-"model": "C200",
-"year": "2024",
-"owner": "Hoang Hai"
-}
+# car = {
+# "brand": "BMW",
+# "model": "C200",
+# "year": "2024",
+# "owner": "Hoang Hai"
+# }
 
-car['type'] = "4-wheel"
-print(car)
+# car['type'] = "4-wheel"
 
-for item in car.values():
-    print(item)
+# # for item in car.values():
+# #     print(item)
 
-for k,v in car.items():
-    print(f"Key is {k} and its value is: {v}")
+# # for k,v in car.items():
+# #     print(f"Key is {k} and its value is: {v}")
+
+
+# car.update({"is_new": True})
+# print(car)
+
+# car_2 = car.copy()
+# print(car_2)
+
+# car.pop("year")
+# print(car)
+word_count = 0
+word_length = 0
+list_index = None
+nor_list = [78,2,5,7,89,111,"apple","banana","candy","donkey","iloveyou"]
+# for item in nor_list:
+#     if type(item) == str:
+#         for ch in item:
+#             word_count = word_count + 1
+#         if word_length < word_count:
+#             word_length = word_count
+#             list_index = nor_list.index(item)
+#             word_count = 0
+#         else:
+#             word_count = 0
+# print(f"String has the longest length is {nor_list[list_index]} with {word_length}")
+smallest = None
+list_index1 = None
+for item in nor_list:
+    if type(item) == int:
+        if smallest is None or smallest > abs(item):
+            smallest = abs(item)
+            list_index1 = nor_list.index(item)            
+print(f"An integer has a lowest absolute value {nor_list[list_index1]} with {smallest} point")
