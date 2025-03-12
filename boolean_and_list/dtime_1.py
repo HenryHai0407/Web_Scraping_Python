6ebf7908481f528ac10e6316658a98f894a5fd4c
HenryHai0407 <hoanghai15100@gmail.com>
2025-03-12 12:59:25 +0200

    add tuple_1.py

---
+++ boolean_and_list/dtime_1.py
@@ -0,0 +1,26 @@
+from datetime import datetime, date
+import pytz
+
+now = datetime.now()
+print(now)
+
+# now1 = date.today()
+# print(now1)
+# datetime1 = datetime(year=2025, month=1, day=25)
+# print(datetime1)
+
+# print(datetime1.month)
+
+# now_str = now.strftime("%d/%m/%Y")
+# print(now_str)
+
+# datetime2 = "26-01-2025"
+# datetime3 = datetime.strptime(datetime2,"%d-%m-%Y")
+# print(datetime3)
+# print(datetime3.year)
+# print(datetime3.month)
+
+# Timezone
+tz_new_york = pytz.timezone("Asia/Saigon")
+datetime_newyork = datetime.now(tz_new_york)
+print(datetime_newyork)
--- boolean_and_list/tuple_1.py
+++ boolean_and_list/tuple_1.py
@@ -1,30 +1,67 @@
-fruits = ("apple","banana","pineapple","kiwi","orange","apple","kiwi")
+# fruits = ("apple","banana","pineapple","kiwi","orange","apple","kiwi")
 
-# ft = set(fruit)
-# #1
-# print(ft)
-# count = 0
-# for item in ft:
-#     count += 1
-# print(count)
+# # ft = set(fruit)
+# # #1
+# # print(ft)
+# # count = 0
+# # for item in ft:
+# #     count += 1
+# # print(count)
 
-#2
-# f_list = ()
-# for idx, item in enumerate(fruit):
-#     fruit_count = (item,fruit.count(item))
-#     fruit_count = tuple(fruit_count)
-#     f_list += fruit_count
-# print(f_list)
+# #2
+# # f_list = ()
+# # for idx, item in enumerate(fruit):
+# #     fruit_count = (item,fruit.count(item))
+# #     fruit_count = tuple(fruit_count)
+# #     f_list += fruit_count
+# # print(f_list)
 
-fruit_counts = {}
-for fruit in fruits:
-    fruit_counts[fruit] = fruit_counts.get(fruit,0) + 1
+# fruit_counts = {}
+# for fruit in fruits:
+#     fruit_counts[fruit] = fruit_counts.get(fruit,0) + 1
 
-num_types = len(fruit_counts)
+# num_types = len(fruit_counts)
 
-min_fruits = min(fruit_counts, key=fruit_counts.get)
-max_fruits = max(fruit_counts, key=fruit_counts.get)
+# min_fruits = min(fruit_counts, key=fruit_counts.get)
+# max_fruits = max(fruit_counts, key=fruit_counts.get)
+# # 
+# print(f"Total number of fruit types: {num_types}")
+# print(f"Fruit with the highest count: {max_fruits} ({fruit_counts[max_fruits]} times)")
+# print(f"Fruit with the lowest count: {min_fruits} ({fruit_counts[min_fruits]} times)")
 
-print(f"Total number of fruit types: {num_types}")
-print(f"Fruit with the highest count: {max_fruits} ({fruit_counts[max_fruits]} times)")
-print(f"Fruit with the lowest count: {min_fruits} ({fruit_counts[min_fruits]} times)")
+tuple_1 = ("apple","orange","kiwi","pineapple","grapes","watermelon","banana","strawberry")
+tuple_2 = (10,30,40,25,35,76,54,53)
+index = 0
+
+# price_max = 0
+# price_min = 0
+# while index < len(tuple_1):
+#     for item in tuple_1:
+#         print(f"The price of {item} is {tuple_2[index]} dollars")
+#         if price_max == 0 or price_max < tuple_2[index]:
+#             f_max = []
+#             f_max.append(item)
+#             price_max = tuple_2[index]
+#         if price_min == 0 or price_min > tuple_2[index]:
+#             f_min = []
+#             f_min.append(item)
+#             price_min = tuple_2[index]
+#         index += 1
+# print(f"Fruit {" ".join(f_max)} has highest price: {price_max}")
+# print(f"Fruit {" ".join(f_min)} has lowest price: {price_min}")
+
+# Second way: Using Dictionary
+# dict1 = {}
+# index = 0
+# while index < len(tuple_1):
+#     for item in tuple_1:
+#         dict1[item] = dict1.get(item,0) + tuple_2[index]
+#         index += 1
+# print(dict1)
+
+# m = max(dict1, key=dict1.get)
+# print(f"The price of {m} is highest with {dict1[m]}")
+# min = min(dict1, key=dict1.get)
+# print(f"The price of {min} is lowest with {dict1[min]}")
+
+# ------------------------------------------
