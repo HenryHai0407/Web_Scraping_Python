ace2b9cabe129a89eff6a050ba043cdfe603a3bd
HenryHai0407 <hoanghai15100@gmail.com>
2025-03-14 14:09:43 +0200

    update dict_1.py exercise

--- dictionary1/dict_1.py
+++ dictionary1/dict_1.py
@@ -13,4 +13,34 @@ dict_1 = {
 # for key in dict_1:
 #     print(key, ":",dict_1[key])
 
-print("\n".join(map(lambda kv: f" {kv[0]}: {kv[1]}", dict_1.items())))
+# print("\n".join(map(lambda kv: f" {kv[0]}: {kv[1]}", dict_1.items())))
+
+students = [
+    {"name": "Alice", "age": 20},
+    {"name": "Bob", "age": 22},
+    {"name": "Lan", "age": 21},
+    {"name": "Charlie", "age": 23},
+    {"name": "Bob", "age": 24},
+    {"name": "David", "age": 22},
+    {"name": "Eve", "age": 20},
+    {"name": "Li", "age": 27},
+    {"name": "An", "age": 26},
+    {"name": "Steven", "age": 21},
+    {"name": "Marry", "age": 23},
+]
+
+age_group = {}
+for student in students:
+    age = student["age"]
+    if age in age_group:
+        age_group[age].append(student)
+    else:
+        age_group[age] = [student]
+
+duplicate_ages = {age: group for age, group in age_group.items() if len(group) > 1}
+print(duplicate_ages)
+
+for age, separated_group in duplicate_ages.items():
+    print(f"For the group has age {age}: ")
+    for sli in separated_group:
+        print(f"{sli['name']}")
