e3ca90f100cb8a981d9b71659e8deaf980959b1e
HenryHai0407 <hoanghai15100@gmail.com>
2025-03-17 13:28:23 +0200

    create a "Student" class in 1 class_1.py

--- class_object/class_1.py
+++ class_object/class_1.py
@@ -1,2 +1,14 @@
-class Car():
+class Student:
+    # class Attribute
+    def __init__(self,name,age):
+        self.name = name # instance Attribute
+        self.age = age
+    def introduce1(self):
+        print(f"Name: {self.name}, Age: {self.age}")
     
+
+
+
+if __name__ == "__main__":
+    student1 = Student("Tuan",12)
+    student1.introduce1()
