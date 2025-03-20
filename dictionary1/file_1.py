03d7dd0eb090e09048701c796feada98cdd046b7
HenryHai0407 <hoanghai15100@gmail.com>
2025-03-20 23:40:18 +0200

    add numbers.txt

--- dictionary1/file_1.py
+++ dictionary1/file_1.py
@@ -1,18 +1,19 @@
 # Working with File in Python
 #
-file_name = "dictionary1\sinhvien.txt"
+# file_name = "dictionary1\sinhvien.txt"
 
-try:
-    with open(file_name,"r") as f:
-        reading_f = f.readline() # Read a first line
-        reading_f1 = f.readline() # If we alreay run readline(), this line will read the NEXT line
-        r2 = f.readlines() # Return a LIST of all sentences
-        print(reading_f)
-        print(f"-------------")
-        print(reading_f1)
-        print(r2)
-except FileNotFoundError:
-    print(f"Error: The file {file_name} does not exist")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# try:
+#     with open(file_name,"r") as f:
+#         reading_f = f.readline() # Read a first line
+#         reading_f1 = f.readline() # If we alreay run readline(), this line will read the NEXT line
+#         r2 = f.readlines() # Return a LIST of all sentences
+#         print(reading_f)
+#         print(f"-------------")
+#         print(reading_f1)
+#         print(r2)
+# except FileNotFoundError:
+#     print(f"Error: The file {file_name} does not exist")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
 
+---
