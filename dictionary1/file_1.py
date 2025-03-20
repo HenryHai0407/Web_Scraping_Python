ab2328d36d8417f7b1ac7e9949c08a8dee08e12c
HenryHai0407 <hoanghai15100@gmail.com>
2025-03-21 00:29:36 +0200

    Finish File test with file_1.py

--- dictionary1/file_1.py
+++ dictionary1/file_1.py
@@ -30,3 +30,51 @@ for line in reading_f1:
     for number in line1:
         number_list.append(number)
 print(number_list)
+
+print(f"Maximum number is {max(number_list)}")
+print(f"Minimum number is {min(number_list)}")
+
+number_dict = {}
+for number in number_list:
+    if number in number_dict.keys():
+        number_dict[number] += 1
+    else:
+        number_dict[number] = 1
+
+max_frequency = []
+for k,v in number_dict.items():
+    if v == max(number_dict.values()):
+        max_frequency.append(k)
+print(", ".join(max_frequency),f"is(are) numbers with highest occurencies")
+
+no_frequency = []
+for k,v in number_dict.items():
+    if v == 1:
+        no_frequency.append(k)
+
+no_frequency_sorted = sorted(no_frequency,reverse=True)
+print(no_frequency_sorted)
+index = 0
+top_10 = []
+for number in no_frequency_sorted:
+    if index < 10:
+        number = int(number)
+        top_10.append(number)
+        index += 1
+
+print(f"The average number of top 10 highest numbers with no duplicate is {round(sum(top_10)/len(top_10),0)}")
+
+no_frequency_sorted_ascending = sorted(no_frequency)
+print(no_frequency_sorted_ascending)
+index1 = 0
+top_10_as = []
+for number in no_frequency_sorted_ascending:
+    if index1 < 10:
+        if number == '0':
+            continue
+        else:
+            number = int(number)
+            top_10_as.append(number)
+            index1 += 1
+print(top_10_as)
+print(f"The average number of top 10 lowest numbers with no duplicate is {round(sum(top_10_as)/len(top_10_as),0)}")
