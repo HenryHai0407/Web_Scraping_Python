d0a114a38bb97591f8edbc7fe614dbfecf9d40d5
HenryHai0407 <hoanghai15100@gmail.com>
2025-04-27 23:52:36 +0300

    small project with great expectation in python

---
+++ great_expectation/ge.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import random
+
+
+# Generate 100 fake user signups
+data = {
+    "user_id": [i for i in range(1, 101)],
+    "email": [f"user{i}@example.com" if random.random() > 0.05 else None for i in range(1, 101)],
+    "age": [random.randint(15, 110) for _ in range(100)],
+    "country": [random.choice(["USA", "Canada", "UK", "Australia", "Unknown"]) for _ in range(100)],
+}
+
+df = pd.DataFrame(data)
+print(df)
