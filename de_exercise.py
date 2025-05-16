59d27ec193a005af0c3bff784ad0f5e54bd1898c
HenryHai0407 <hoanghai15100@gmail.com>
2025-05-17 01:01:45 +0300

    update de_exercise v2

--- de_exercise.py
+++ de_exercise.py
@@ -30,3 +30,30 @@
 
 #3 mutta toinen ratkaisu:
 print([x for x in "Huynh Ngoc Phien" if x != " " and x not in ('a','e','i','o','u','y')])
+
+####4
+olt = []
+ostr = "Huynh Ngoc Phien"
+for x in ostr:
+    olt.append(x)
+olt.reverse()
+
+stradd = ""
+for x in olt:
+    stradd += x
+print(stradd)
+
+####5
+list2 = [4,7,2,3,5,9,10]
+a = 0
+for x in list2:
+    if x >= a:
+        a = x
+    else:
+        continue
+print(a)
+
+###5 toinen ratkaisu:
+from functools import reduce
+b = reduce(lambda x,y: x if x>=y else y, [4, 7, 2, 3, 5, 9, 10], 0)
+print(b)
